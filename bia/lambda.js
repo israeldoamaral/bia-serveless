65962765dc454bdc9e6d97897728dd9b3d154492
israeldoamaral <israeldoamaral@gmail.com>
2026-01-27 18:11:58 -0300

    projeto rodando em nova rede + bia com react na porta 3000

--- bia/lambda.js
+++ bia/lambda.js
@@ -1,3 +1,3 @@
-const serverlessExpress = require('@codegenie/serverless-express')
-const app = require('./config/express')()
-exports.handler = serverlessExpress({ app })
+const serverlessExpress = require('@codegenie/serverless-express');
+const app = require('./config/express')();
+exports.handler = serverlessExpress({ app });
